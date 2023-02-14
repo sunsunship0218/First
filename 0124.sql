6a8fa7d3be6d11943a76627499dd517e88221d22
sunsunship0218 <114408002+sunsunship0218@users.noreply.github.com>
2023-02-14 18:58:12 +0800

    Add files via upload

--- 0124.sql
+++ 0124.sql
@@ -20,10 +20,30 @@ USE `First_DataBase` ;
 CREATE TABLE `student`
 (
 `name` VARCHAR(20) NOT NULL,
-`major` VARCHAR(20) UNIQUE
+`major` VARCHAR(20) UNIQUE,
+`ID` INT AUTO_INCREMENT,
+PRIMARY KEY(`ID`)
 );
-INSERT INTO `student` VALUE ('A_','english');
-INSERT INTO `student` VALUE ('B_','chinese');
+INSERT INTO `student` VALUE ('A_','english',2,'pass');
+INSERT INTO `student`(`name`,`major`) VALUES('D_','french');
+INSERT INTO `student`(`name`,`major`) VALUES('B_','Math');
 ALTER TABLE `student` ADD `pass` VARCHAR(20) DEFAULT 'not pass';
 DESCRIBE `student`;
 SELECT * FROM `student`;
+UPDATE `student`
+SET `major`='French speaking'
+WHERE `major`='french';
+SET SQL_SAFE_UPDATES=0;
+UPDATE `student`
+SET `name`='vu' 
+WHERE `ID`=1;
+SET SQL_SAFE_UPDATES=0;
+DELETE FROM `student`
+WHERE `ID`=3;
+
+SELECT `name`,`ID` FROM `student`;
+
+
+
+
+
